c8fbc2207c4296d2b3cc18dfc833053e2dec40be
Emmanuel Okokon Udo <ubryliant@gmail.com>
2023-06-23 00:18:23 +0100

    update joam version

---
+++ edu.js
@@ -0,0 +1,64 @@
+const navToggler = document.querySelector('.hambugger');
+const navList = document.querySelector('.nav__list');
+const nav_list = document.querySelectorAll('.list');
+const navBar = document.querySelector('.nav__bar');
+
+
+
+
+window.addEventListener('load', () => {
+    document.querySelector('.preloader').style.display = 'none'  
+})
+
+function activateScrolly() {
+    navBar.classList.toggle('addNavBarColor', scrollY > 20)
+}
+
+//activates the navigation bar and controls the hambugger movement
+function activateNavbar() {
+    let navState = navToggler.getAttribute('aria-controls');
+if(navState === 'closed'){
+    navToggler.setAttribute('aria-controls', 'open')
+}else{navToggler.setAttribute('aria-controls', 'closed')}
+navList.classList.toggle('navActive')
+}
+
+//deacivates the navbar on scroll with a corresponding hambugger movement
+function removeNavbar() {
+    let navState = navToggler.getAttribute('aria-controls');
+if(navState === 'open'){
+    navToggler.setAttribute('aria-controls', 'closed')
+    navList.classList.remove('navActive')
+}
+}
+
+//declare active main navigation button on click
+function declareActiveMainNav() {
+    Array.from(nav_list).forEach(nav => {
+        nav.addEventListener('click', () => {
+            nav_list.forEach(nav => {
+                nav.classList.remove('active')
+            })
+          nav.classList.add('active')
+        })
+    })
+}
+
+
+declareActiveMainNav();
+
+window.addEventListener('scroll', () => {
+    activateScrolly();
+})
+
+window.addEventListener('load', () => {
+    navList.classList.remove('navActive')  
+})
+
+navToggler.addEventListener('click', () => {
+    activateNavbar()
+})
+
+window.addEventListener('scroll', () => {
+    removeNavbar()    
+})
